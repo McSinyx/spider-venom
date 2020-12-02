09a68de701c90ef61abdac17fe21c35e85cdac49
NhacBatQuan <59557905+NhacBatQuan@users.noreply.github.com>
2020-12-02 14:32:13 +0700

    Update thanhnien.py

--- spider/thanhnien.py
+++ spider/thanhnien.py
@@ -19,6 +19,7 @@ def articles(links):
         url = 'http://thanhnien.vn/' + href
         if url.endswith('.html') and 'vac' in url: yield url
 
+            
 async def download(caption, url, dest, client):
     """The image and caption saved if contain information about vaccine"""
     name, ext = splitext(basename(urlparse(url).path))
@@ -33,18 +34,20 @@ async def download(caption, url, dest, client):
         async for chunk in fi.aiter_bytes(): await fo.write(chunk)
     await (directory/'caption').write_text(caption, encoding='utf-8')
     print(caption)
+    
 
 async def scrape_images(url, dest, client, nursery):
     """Search for img in the articles in order to download the images."""
     article = await client.get(url)
     for img in parse_html5(article.text).iterfind('.//img'):
         caption, url = img.get('alt'), img.get('data-src')
-        if caption is None or ('vắc') not in caption.lower(): 
-           if caption is None or ('vac') not in caption.lower(): continue
+        if caption is None or ('vắc') not in caption.lower():
+            if caption is None or ('vac') not in caption.lower(): continue
         if url is None: url = img.get('src')
         if url.endswith('logo.svg'): continue
         nursery.start_soon(download, caption, url, dest, client)
 
+        
 async def thanhnien(dest, client, nursery):
     """scrape image and captions optain from link."""
     index = await client.get(INDEX)
